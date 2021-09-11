4fb0881a97474fbabb264a48a069c7a155dd668c
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2021-09-11 16:15:49 -0700

    feat: add local user role creation script

--- server/lib/schema.sql
+++ server/lib/schema.sql
@@ -1,3 +1,17 @@
+-- Create Role: perrnguser
+-- DROP ROLE perrnguser;
+
+CREATE ROLE perrnguser WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  CREATEDB
+  CREATEROLE
+  REPLICATION
+  ENCRYPTED PASSWORD 'md5b4f1d5207278e396a6899040e49af8df';
+
+GRANT postgres TO perrnguser WITH ADMIN OPTION;
+
 -- Drops the DB if it exists currently --
 DROP DATABASE IF EXISTS perrng-app;
 -- Creates the DB --
