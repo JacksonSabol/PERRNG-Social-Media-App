473868498fedd761196c8c7f201a3adfdca1fb0a
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2021-09-11 16:25:20 -0700

    feat: add User table sequelize model

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    /**
+     * The variable name User will get attached to the db object for querying the database
+     * while the table name 'User' defined in single quotes is what appears in the database
+     */
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            validate: {
+                isEmail: true,
+                message: (props) => `${props.value} is not a valid email address!`,
+            },
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.CHAR(60),
+            allowNull: false,
+        },
+    }, {
+        freezeTableName: true,
+    });
+    // User.associate = (models) => {
+    //     User.hasMany(models.Post);
+    // };
+    return User;
+};
